029f4203fd19102296d6e63a1c683ffa228c8bb0
danshapero <shapero.daniel@gmail.com>
2017-04-29 16:26:44 -0700

    Removed golden section search

--- include/icepack/numerics/optimization.hpp
+++ include/icepack/numerics/optimization.hpp
@@ -80,63 +80,6 @@ namespace icepack
       return secant_search(f, df, a, b, armijo, wolfe, 0, log);
     }
 
-
-    /**
-     * Find the minimum of a 1D, convex function `f` within an interval `[a, b]`
-     * using Golden section search.
-     */
-    template <typename Functional>
-    double golden_section_search(
-      Functional&& f,
-      double a,
-      double b,
-      const double eps
-    )
-    {
-      assert(b > a);
-      const double phi = 0.5 * (std::sqrt(5.0) - 1);
-      const double d = b - a;
-
-      while (b - a > eps * d)
-      {
-        const double L = b - a;
-        const double A = a + L * (1 - phi);
-        const double B = b - L * (1 - phi);
-        const double fA = f(A), fB = f(B);
-
-        /* There are four cases to consider if `f` is convex. In these two
-         * cases, the minimum lies in the interval [A, b]:
-         *  ^                 ^
-         *  |  o              |  o
-         *  |                 |          o
-         *  |    o            |    o
-         *  |      o          |       o
-         *  |          o      |
-         *  *------------>    *------------>
-         *     a A B   b         a A  B  b
-         *
-         * and in these two cases, the minimum lies in the interval [a, B].
-         * ^                  ^
-         * |         o        |         o
-         * |                  | o
-         * |       o          |       o
-         * |     o            |    o
-         * | o                |
-         * *------------->    *------------>
-         *   a   A B b          a  A  B  b
-         *
-         * These cases are characterized by whether f(A) >= f(B) or vice versa.
-         */
-
-        if (fA >= fB)
-          a = A;
-        else
-          b = B;
-      }
-
-      return (a + b) / 2;
-    }
-
   } // namespace numerics
 
 } // namespace icepack
--- test/optimization.cpp
+++ test/optimization.cpp
@@ -5,21 +5,12 @@
 
 int main()
 {
-
   const double x0 = 3.0;
   const auto f = [=](const double x) { return std::cosh(x - x0); };
   const auto df = [=](const double x) { return std::sinh(x - x0); };
 
   const double a = 0.0;
   const double b = 10.0;
-
-
-  TEST_SUITE("golden section search")
-  {
-    const double x = icepack::numerics::golden_section_search(f, a, b, 1.0e-6);
-    CHECK_REAL(x, x0, 1.0e-4);
-  }
-
 
   TEST_SUITE("secant search")
   {
